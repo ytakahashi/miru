8a67151b3590a8c8db2400781a8ad54b0b43eae8
ytakahashi <yt01tc@gmail.com>
2021-01-01 10:16:54 +0900

    list issues (#5)

--- src/domain/githubRepositoryService.ts
+++ src/domain/githubRepositoryService.ts
@@ -1,24 +1,11 @@
 import { LocalStorageAccessor } from '@/domain/interface/localStorageAccessor'
 import { GitHubAccessor } from '@/domain/interface/githubAccessor'
 import { newGitHubAccessor, newLocalStorageAccessor } from '@/domain/interface/factory'
-import { Repository } from '@/model/githubRepository'
-
-const regex = /^https:\/\/(?<origin>[^/]+)\/(?<owner>[^/]+)\/(?<name>.+)$/
+import { Repository, RepositoryUrl } from '@/model/githubRepository'
 
 type repository = {
   owner: string;
   name: string;
-}
-
-const parseRepositoryUrl = (url: string): repository|null => {
-  const result = url.match(regex)
-  if (result?.groups !== undefined) {
-    return {
-      owner: result.groups.owner,
-      name: result.groups.name
-    }
-  }
-  return null
 }
 
 export class GitHubRepositoryService {
@@ -30,17 +17,12 @@ export class GitHubRepositoryService {
     this.localStorageAccessor = newLocalStorageAccessor()
   }
 
-  async getIssues (url: string): Promise<Repository|undefined> {
-    if (this.githubAccessor === undefined) {
+  async getIssues (url: RepositoryUrl): Promise<Repository|undefined> {
+    if (this.githubAccessor === undefined || !url.isValid()) {
       return undefined
     }
 
-    const repo = parseRepositoryUrl(url)
-    if (repo === null) {
-      return undefined
-    }
-
-    const issues = await this.githubAccessor.getIssues(repo.owner, repo.name)
+    const issues = await this.githubAccessor.getIssues(url.getOwner(), url.getName())
     return issues
   }
 }
--- src/model/githubRepository.ts
+++ src/model/githubRepository.ts
@@ -3,8 +3,13 @@ export type IssueLabel = {
   color: string;
 }
 
+export type IssueLabelNode = {
+  node: IssueLabel;
+}
+
 export type IssueLabelEdge = {
-  edges: Array<IssueLabel>;
+  cursor?: string;
+  edges: Array<IssueLabelNode>;
 }
 
 export type Issue = {
@@ -30,4 +35,42 @@ export type GithubRepository = {
 
 export type Repository = {
   repository: GithubRepository;
+}
+
+export class RepositoryUrl {
+  private urlRegex = /^https:\/\/(?<origin>[^/]+)\/(?<owner>[^/]+)\/(?<name>.+)$/
+  private readonly url: string;
+  private readonly owner?: string;
+  private readonly name?: string;
+
+  constructor (url: string) {
+    this.url = url
+    const result = url.match(this.urlRegex)
+    if (result?.groups !== undefined) {
+      this.owner = result.groups.owner
+      this.name = result.groups.name
+    }
+  }
+
+  public isValid (): boolean {
+    return this.name !== undefined && this.owner !== undefined
+  }
+
+  public getUrl (): string {
+    return this.url
+  }
+
+  public getOwner (): string {
+    if (this.owner === undefined) {
+      throw new Error('Invalid URL')
+    }
+    return this.owner
+  }
+
+  public getName (): string {
+    if (this.name === undefined) {
+      throw new Error('Invalid URL')
+    }
+    return this.name
+  }
 }
