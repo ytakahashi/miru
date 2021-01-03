0abf4be5cb409f7e9493eb67c69fda0e77b6dff2
ytakahashi <yt01tc@gmail.com>
2021-01-03 20:05:37 +0900

    lint disable

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,6 @@
+/* eslint-disable */
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const component: DefineComponent<{}, {}, any>
   export default component
 }
