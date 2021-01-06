654a459fbe1bcc81ffbed54888d0cf4a250da05f
ytakahashi <yt01tc@gmail.com>
2021-01-07 06:49:33 +0900

    update issue list design (#13)

--- src/components/GitHubIssue.vue
+++ src/components/GitHubIssue.vue
@@ -1,24 +1,32 @@
 <template>
-  <h3>{{ repositoryUrl.asString() }}</h3>
-  <button v-on:click="getIssues()">list issues</button>
+  <div class="issue-list">
+    <div class="issue-list-header">
+      <span class="repository-name clickable" v-on:click="openRepository()">{{ repositoryUrl.asString() }}</span>
+      <button type="button" v-on:click="getIssues()">
+        <i class="fas fa-sync-alt"></i>
+      </button>
+    </div>
 
-  <div v-for="issue in issues" :key="issue.url">
-    <IssueContent :issue="issue" />
-  </div>
+    <div v-for="issue in issues" :key="issue.url">
+      <IssueContent :issue="issue" />
+    </div>
 
-  <div v-if="isEmpty">
-    No issues.
-  </div>
+    <div v-if="isEmpty" class="clickable" v-on:click="openIssues()">
+      No issues.
+    </div>
 
-  <div v-if="isFailed">
-    Failed to list issues of {{ repositoryUrl.getUrl() }}
+    <div v-if="isFailed" class="clickable" v-on:click="openIssues()">
+      Failed to list issues of {{ repositoryUrl.getUrl() }}.
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import IssueContent, { Issue, Label } from '@/components/IssueContent.vue'
+import { shell } from 'electron'
+import IssueContent from '@/components/IssueContent.vue'
 import { GitHubRepositoryService } from '@/domain/githubRepositoryService'
+import { Issue, IssueLabel } from '@/model/github'
 import { RepositoryUrl } from '@/model/githubRepository'
 
 type DataType = {
@@ -54,7 +62,7 @@ export default defineComponent({
       const response = await this.githubRepositoryService.getIssues(this.repositoryUrl)
       const issues = response?.repository?.issues.edges
         .map(v => v.node)
-        .map(v => new Issue(v.title, v.url, v.createdAt, v.updatedAt, v.labels.edges.map(l => new Label(l.node.name, l.node.color))))
+        .map(v => new Issue(v.title, v.url, v.createdAt, v.updatedAt, v.labels.edges.map(l => new IssueLabel(l.node.name, l.node.color))))
       if (issues === undefined) {
         this.isFailed = true
       } else {
@@ -62,7 +70,42 @@ export default defineComponent({
         this.issues = issues
         this.isEmpty = issues.length === 0
       }
+    },
+    openRepository (): void {
+      shell.openExternal(this.repositoryUrl.getUrl())
+    },
+    openIssues (): void {
+      shell.openExternal(`${this.repositoryUrl.getUrl()}/issues`)
     }
   }
 })
 </script>
+
+<style scoped lang="scss">
+.repository-name {
+  font-size: 110%;
+  font-weight: bold;
+  padding-left: 0.4em;
+  padding-right: 0.4em;
+}
+
+.issue-list {
+  width: 70%;
+  margin: 0 auto;
+  margin-bottom: 1.5em;
+  padding: 1em;
+  position: relative;
+  border: 1px solid #a9a9a9;
+  border-radius: 8px;
+}
+
+.issue-list-header{
+  position: absolute;
+  top: -0.7em;
+  background: #ffffff;
+}
+
+.clickable {
+  cursor: pointer;
+}
+</style>
