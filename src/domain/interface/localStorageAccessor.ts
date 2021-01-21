c06eab978b3681af38bbf05a227c4d9f723f378a
ytakahashi <yt01tc@gmail.com>
2021-01-22 06:02:40 +0900

    update

--- src/domain/interface/localStorageAccessor.ts
+++ src/domain/interface/localStorageAccessor.ts
@@ -1,5 +1,4 @@
 import { ApplicationSetting } from '@/domain/model/application'
-import { GitHubUser } from '@/infrastructure/dto/githubApi'
 import { GitHubAccount } from '@/infrastructure/dto/local'
 
 export type RepositorySetting = {
@@ -12,16 +11,8 @@ export interface LocalStorageAccessor {
   getPath (): string;
   setApplicationSettings (settings: Array<ApplicationSetting>): void;
   getApplicationSettings (): Array<ApplicationSetting>;
-  setPersonalAccessToken (pat: string): void;
-  getPersonalAccessToken (): string | undefined;
-  setUser (user: GitHubUser): void;
-  getUser (): GitHubUser | undefined;
   setGitHubAccount (account: GitHubAccount): void;
   getGitHubAccount (): GitHubAccount | undefined;
-  setGitHubRepositoryUrls (urls: Array<string>): void;
-  getGitHubRepositoryUrls (): Array<string>;
-  deleteGitHubRepositoryUrls (): void;
   setRepositorySettings (settings: Array<RepositorySetting>): void;
   getRepositorySettings (): Array<RepositorySetting>;
-  deleteRepositorySettings (): void;
 }
--- src/infrastructure/impl/electronStoreWrapper.ts
+++ src/infrastructure/impl/electronStoreWrapper.ts
@@ -1,14 +1,10 @@
 import Store from 'electron-store'
-import { LocalStorageAccessor, RepositorySetting } from '@/domain/interface/localStorageAccessor'
+import { LocalStorageAccessor } from '@/domain/interface/localStorageAccessor'
 import { ApplicationSetting } from '@/domain/model/application'
-import { GitHubUser } from '@/infrastructure/dto/githubApi'
-import { GitHubAccount } from '@/infrastructure/dto/local'
+import { GitHubAccount, RepositorySetting } from '@/infrastructure/dto/local'
 
 type StoreType = {
-  personalAccessToken?: string;
-  user?: GitHubUser;
   account?: GitHubAccount;
-  githubRepositoryUrls?: Array<string>;
   applicationSettings?: Array<ApplicationSetting>;
   repositorySettings?: Array<RepositorySetting>;
 }
@@ -36,41 +32,12 @@ export class ElectronStoreWrapper implements LocalStorageAccessor {
     return s === undefined ? [] : s
   }
 
-  setPersonalAccessToken = (pat: string): void => {
-    this.#store.set('personalAccessToken', pat)
-  }
-
-  getPersonalAccessToken = (): string | undefined => {
-    return this.#store.get('personalAccessToken')
-  }
-
-  setUser = (user: GitHubUser): void => {
-    this.#store.set('user', user)
-  }
-
-  getUser = (): GitHubUser | undefined => {
-    return this.#store.get('user')
-  }
-
   setGitHubAccount = (account: GitHubAccount): void => {
     this.#store.set('account', account)
   }
 
   getGitHubAccount = (): GitHubAccount | undefined => {
     return this.#store.get('account')
-  }
-
-  setGitHubRepositoryUrls = (urls: Array<string>): void => {
-    this.#store.set('githubRepositoryUrls', urls)
-  }
-
-  getGitHubRepositoryUrls = (): Array<string> => {
-    const urls = this.#store.get('githubRepositoryUrls')
-    return urls === undefined ? [] : urls
-  }
-
-  deleteGitHubRepositoryUrls = (): void => {
-    this.#store.delete('githubRepositoryUrls')
   }
 
   setRepositorySettings = (settings: Array<RepositorySetting>): void => {
@@ -80,9 +47,5 @@ export class ElectronStoreWrapper implements LocalStorageAccessor {
   getRepositorySettings = (): Array<RepositorySetting> => {
     const settings = this.#store.get('repositorySettings')
     return settings === undefined ? [] : settings
-  }
-
-  deleteRepositorySettings = (): void => {
-    this.#store.delete('repositorySettings')
   }
 }
