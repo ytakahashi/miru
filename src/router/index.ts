c3c08b46381ab78f9d51ccbd1a1374d92db631ec
ytakahashi <yt01tc@gmail.com>
2021-01-23 10:40:41 +0900

    update issue/PR view (#35)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,5 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import { AccountSettingUseCaseFactoryImpl, ApplicationSettingUseCaseFactoryImpl, RepositorySettingUseCaseFactoryImpl } from '@/usecase/factory/useCaseFactory'
 import SettingView from '../views/SettingView.vue'
-
-const accountSettingUseCaseFactory = new AccountSettingUseCaseFactoryImpl()
-const applicationSettingUseCase = new ApplicationSettingUseCaseFactoryImpl().newApplicationSettingUseCase()
-const repositorySettingUseCaseFactory = new RepositorySettingUseCaseFactoryImpl()
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -15,22 +10,12 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/issues',
     name: 'Issues',
-    component: () => import(/* webpackChunkName: "issues" */ '../views/IssuesView.vue'),
-    props: {
-      accountSettingUseCaseFactory: accountSettingUseCaseFactory,
-      applicationSettingUseCase: applicationSettingUseCase,
-      repositorySettingUseCaseFactory: repositorySettingUseCaseFactory
-    }
+    component: () => import(/* webpackChunkName: "issues" */ '../views/IssuesView.vue')
   },
   {
     path: '/pulls',
     name: 'PullRequests',
-    component: () => import(/* webpackChunkName: "pulls" */ '../views/PullRequestsView.vue'),
-    props: {
-      accountSettingUseCaseFactory: accountSettingUseCaseFactory,
-      applicationSettingUseCase: applicationSettingUseCase,
-      repositorySettingUseCaseFactory: repositorySettingUseCaseFactory
-    }
+    component: () => import(/* webpackChunkName: "pulls" */ '../views/PullRequestsView.vue')
   }
 ]
 
--- src/views/PullRequestsView.vue
+++ src/views/PullRequestsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <ListOption :viewType="'pullRequests'" />
+  <QueryOption :viewType="'pullRequests'" />
   <div v-for="(t, index) in tuples" :key="index">
     <div v-for="repositorySetting in t.repositorySettings" :key="repositorySetting.getUrl()">
       <GitHubPullRequest
@@ -8,19 +8,67 @@
       />
     </div>
   </div>
-  <div v-if="tuples.length === 0">Account is not configured.</div>
+  <div v-if="!isAccountConfigured">Account is not configured.</div>
+  <div v-if="isAccountConfigured && total === 0">No repositories are configured.</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent, onMounted, ref, Ref } from 'vue'
 import GitHubPullRequest from '@/components/GitHubPullRequest.vue'
-import RepositoryViewBase from '@/views/repositoryViewBase'
+import QueryOption from '@/components/QueryOption.vue'
+import { inject } from '@/di/injector'
+import { AccountSettingUseCaseFactoryKey, ApplicationSettingUseCaseKey, RepositorySettingUseCaseFactoryKey } from '@/di/types'
+import { Account } from '@/domain/model/github'
+import { RepositorySetting } from '@/domain/model/githubRepository'
+
+type RepositoryTuple = {
+  account: Account;
+  repositorySettings: Array<RepositorySetting>;
+}
 
 export default defineComponent({
-  extends: RepositoryViewBase,
   name: 'PullRequestView',
   components: {
-    GitHubPullRequest
+    GitHubPullRequest,
+    QueryOption
+  },
+  setup () {
+    const accountSettingUseCaseFactory = inject(AccountSettingUseCaseFactoryKey)
+    const applicationSettingUseCase = inject(ApplicationSettingUseCaseKey)
+    const repositorySettingUseCaseFactory = inject(RepositorySettingUseCaseFactoryKey)
+
+    const tuples: Ref<RepositoryTuple[]> = ref([])
+
+    const initAccounts = () => {
+      const settings = applicationSettingUseCase.getSettings()
+      for (const s of settings) {
+        const accountSettingUseCase = accountSettingUseCaseFactory.newAccountSettingUseCase(s)
+        const repositorySettingUseCase = repositorySettingUseCaseFactory.newRepositorySettingUseCase(s)
+        const account = accountSettingUseCase.getAccount()
+        const repositorySettings = repositorySettingUseCase.getRepositorySettings()
+        tuples.value.push({
+          account: account,
+          repositorySettings: repositorySettings.filter(s => s.showsPullRequests())
+        })
+      }
+    }
+
+    const total = computed(() =>
+      tuples.value
+        .map(v => v.repositorySettings)
+        .map(v => v.length)
+        .reduce((a, b) => a + b, 0)
+    )
+
+    const isAccountConfigured = computed(() => tuples.value.length !== 0)
+
+    onMounted(initAccounts)
+
+    return {
+      isAccountConfigured,
+      total,
+      tuples
+    }
   }
 })
 </script>
