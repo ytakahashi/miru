5e95b95828755e23660df47bcbed8a0ddead9399
ytakahashi <yt01tc@gmail.com>
2021-01-31 20:32:34 +0900

    chore: bump dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transform: {
     '^.+\\.vue$': 'vue-jest'
-  }
+  },
+  modulePathIgnorePatterns: [
+    '<rootDir>/build',
+    '<rootDir>/dist_electron'
+  ]
 }
--- src/background.ts
+++ src/background.ts
@@ -5,6 +5,10 @@ import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Store = require('electron-store')
+Store.initRenderer()
+
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
